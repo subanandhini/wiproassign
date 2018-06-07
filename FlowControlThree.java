f16fa2807faa2b1a27084db31463a11eeceb4986
subanandhini <34816663+subanandhini@users.noreply.github.com>
2018-06-07 10:51:30 +0530

    Create FlowControlThree.java

---
+++ FlowControlThree.java
@@ -0,0 +1,11 @@
+public class FlowControlThree{
+
+	public static void main(String[] args) {
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int add=a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+add);
+	}
+
+}
