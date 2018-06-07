c950788ceb7efef427e8172d58440f6ec3b76dfd
subanandhini <34816663+subanandhini@users.noreply.github.com>
2018-06-07 11:52:50 +0530

    Create IfProgramOne.java

---
+++ IfProgramOne.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IfProgramOne {
+
+	public static void main(String[] args) {
+Scanner sa= new Scanner(System.in);
+int number=sa.nextInt();
+if(number==0) 
+	System.out.println("The given number is Zero");
+if(number<0) 
+	System.out.println("The given number is negative");
+
+	if(number>0)
+		System.out.println("The given number is Positive");
+	}
+	}
+
+
